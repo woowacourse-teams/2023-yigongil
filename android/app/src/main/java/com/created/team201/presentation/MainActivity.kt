063b218947d7cc9d47b09410fb74722a224da419
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-05 17:14:02 +0900

    chore: android initial setting

---
+++ android/app/src/main/java/com/created/team201/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.created.team201.presentation
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityMainBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
