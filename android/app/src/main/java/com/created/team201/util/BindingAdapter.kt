336ef41495b0addb15fa48e8fa39a2b7844d768d
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-05 17:21:47 +0900

    feat: add bindingAdapter util

---
+++ android/app/src/main/java/com/created/team201/util/BindingAdapter.kt
@@ -0,0 +1,26 @@
+package com.created.team201.util
+
+import android.view.View
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+
+object BindingAdapter {
+
+    @JvmStatic
+    @BindingAdapter("isVisible")
+    fun isVisible(view: View, isVisible: Boolean) {
+        when (isVisible) {
+            true -> view.visibility = View.VISIBLE
+            false -> view.visibility = View.INVISIBLE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("glideSrc")
+    fun glideSrc(imageview: ImageView, image: Int) {
+        Glide.with(imageview.context)
+            .load(image)
+            .into(imageview)
+    }
+}
