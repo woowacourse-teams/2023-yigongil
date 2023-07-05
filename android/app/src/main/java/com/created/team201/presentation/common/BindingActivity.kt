9bf912c289ad4c4a0a370593d13895146270af5b
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-05 17:21:30 +0900

    feat: add base class

---
+++ android/app/src/main/java/com/created/team201/presentation/common/BindingActivity.kt
@@ -0,0 +1,19 @@
+package com.created.team201.presentation.common
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BindingActivity<B : ViewDataBinding>(
+    @LayoutRes private val layoutResId: Int,
+) : AppCompatActivity() {
+    private var _binding: B? = null
+    val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = DataBindingUtil.setContentView(this, layoutResId)
+    }
+}
