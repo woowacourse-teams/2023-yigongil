a41cbdc188615d162f77b03c399e7496525638c5
Yerin <myheaven_b@naver.com>
2023-07-12 13:33:15 +0900

    feat: 티어에 해당하는 resource를 연결하는 TierImage 생성, item에서 glide로 이미지 연결

---
+++ android/app/src/main/java/com/created/team201/presentation/studyList/TierImage.kt
@@ -0,0 +1,20 @@
+package com.created.team201.presentation.studyList
+
+import com.created.team201.R
+
+enum class TierImage(private val id: Int, private val resId: Int) {
+    BRONZE(1, R.drawable.ic_bronze_24),
+    SILVER(2, R.drawable.ic_silver_24),
+    GOLD(3, R.drawable.ic_gold_24),
+    PLATINUM(4, R.drawable.ic_platinum_24),
+    DIAMOND(5, R.drawable.ic_diamond_24),
+    ;
+
+    companion object {
+        fun valueOf(id: Int): Int {
+            return TierImage.values().find { image ->
+                image.id == id
+            }?.resId ?: throw IllegalAccessException("해당 티어를 찾을 수 없습니다.")
+        }
+    }
+}
