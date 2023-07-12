e874f5c528998fe3b8df843b056594271f2a88ba
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-12 13:46:06 +0900

    feat: 홈 뷰모델 생성 및 더미데이터 생성

--- android/app/src/main/java/com/created/team201/presentation/home/HomeViewModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/home/HomeViewModel.kt
@@ -1,7 +1,66 @@
 package com.created.team201.presentation.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.created.domain.model.Study
+import com.created.domain.model.Todo
+import com.created.domain.model.UserInfo
+import com.created.team201.presentation.home.model.HomeUiModel
+import com.created.team201.presentation.home.model.TodoUiModel
 
-class HomeViewModel:ViewModel() {
+class HomeViewModel : ViewModel() {
+    private val _userName: MutableLiveData<String> = MutableLiveData()
+    val userName: LiveData<String> = _userName
+    private val _userStudies: MutableLiveData<List<HomeUiModel>> = MutableLiveData()
+    val userStudies: LiveData<List<HomeUiModel>> = _userStudies
 
+    fun getUserStudyInfo() {
+        _userName.value = DUMMY.userName
+        _userStudies.value = DUMMY.studies.map { it.toUiModel() }
+    }
+
+    private fun Study.toUiModel(): HomeUiModel =
+        HomeUiModel(
+            studyName = this.studyName,
+            progressRate = this.progressRate,
+            leftDays = this.leftDays,
+            nextDate = this.nextDate,
+            necessaryTodo = this.necessaryTodo.toUiModel(),
+            optionalTodos = this.optionalTodo.map { it.toUiModel() },
+        )
+
+    private fun Todo.toUiModel(): TodoUiModel = TodoUiModel(
+        todoId = this.todoId,
+        content = this.content,
+        isDone = this.isDone,
+    )
+
+    companion object {
+        private val DUMMY = UserInfo(
+            "산군",
+            listOf(
+                Study(
+                    1,
+                    "빨리 만들자",
+                    80,
+                    5,
+                    "2023.01.02",
+                    Todo(2, "잔디구현끝내기", true),
+                    listOf(Todo(3, "홈뷰 끝내기", false)),
+
+                ),
+                Study(
+                    2,
+                    "빨리 만들자고",
+                    87,
+                    9,
+                    "2023.07.02",
+                    Todo(2, "선택 투두 끝내기", true),
+                    listOf(Todo(3, "홈뷰 끝내기2", true)),
+
+                ),
+            ),
+        )
+    }
 }
