190adcabdf267a6a56afcd1fc2e9918b5889420b
Bandal <no1msh1217@gmail.com>
2023-07-17 23:54:46 +0900

    feat: 스터디 참가자 리스트 어댑터 구현

---
+++ android/app/src/main/java/com/created/team201/presentation/studyList/StudyParticipantsAdapter.kt
@@ -0,0 +1,36 @@
+package com.created.team201.presentation.studyList
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.created.team201.presentation.studyList.uiModel.StudyParticipant
+
+class StudyParticipantsAdapter :
+    ListAdapter<StudyParticipant, StudyPeopleViewHolder>(diffCallBack) {
+
+    init {
+        setHasStableIds(true)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudyPeopleViewHolder {
+        return StudyPeopleViewHolder(StudyPeopleViewHolder.getBinding(parent))
+    }
+
+    override fun onBindViewHolder(holder: StudyPeopleViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    companion object {
+        private val diffCallBack = object : DiffUtil.ItemCallback<StudyParticipant>() {
+            override fun areItemsTheSame(
+                oldItem: StudyParticipant,
+                newItem: StudyParticipant,
+            ): Boolean = oldItem.name == newItem.name
+
+            override fun areContentsTheSame(
+                oldItem: StudyParticipant,
+                newItem: StudyParticipant,
+            ): Boolean = oldItem == newItem
+        }
+    }
+}
