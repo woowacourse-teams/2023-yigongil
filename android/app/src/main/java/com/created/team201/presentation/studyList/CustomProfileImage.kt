ca47da6552e9c3f08f58f18c24f3086d01ae359a
Bandal <no1msh1217@gmail.com>
2023-07-17 23:53:58 +0900

    feat: 프로필 이미지 커스텀 뷰 구현
    1. 원형으로 자른 이미지
    2. 원형 프로그레스 바

---
+++ android/app/src/main/java/com/created/team201/presentation/studyList/CustomProfileImage.kt
@@ -0,0 +1,48 @@
+package com.created.team201.presentation.studyList
+
+import android.content.Context
+import android.content.res.ColorStateList
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
+import com.bumptech.glide.Glide
+import com.created.team201.databinding.CustomProfileImageBinding
+import com.created.team201.presentation.studyList.uiModel.Tier
+
+class CustomProfileImage @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val binding: CustomProfileImageBinding by lazy {
+        CustomProfileImageBinding.inflate(LayoutInflater.from(context), this, true)
+    }
+
+    private val studySuccessRate: ProgressBar by lazy { binding.pbCustomProfileImageSuccessRate }
+    private val profileImage: ImageView by lazy { binding.ivCustomProfileImage }
+
+    fun setGlideCircleImageUrl(image: String) {
+        Glide.with(profileImage.context)
+            .load(image)
+            .circleCrop()
+            .into(profileImage)
+        invalidate()
+        requestLayout()
+    }
+
+    fun setSuccessRate(successRate: Int) {
+        studySuccessRate.progress = successRate
+        invalidate()
+        requestLayout()
+    }
+
+    fun setTier(tierName: String) {
+        val tier = Tier.of(tierName)
+        studySuccessRate.progressTintList = ColorStateList.valueOf(context.getColor(tier.color))
+        invalidate()
+        requestLayout()
+    }
+}
