cb5c95805394b3827137293e5f14942aeb27115d
Bandal <no1msh1217@gmail.com>
2023-07-17 23:58:38 +0900

    feat: 스터디 상세보기 액티비티 구현

---
+++ android/app/src/main/java/com/created/team201/presentation/studyList/StudyDetail.kt
@@ -0,0 +1,47 @@
+package com.created.team201.presentation.studyList
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.viewModels
+import com.created.team201.R
+import com.created.team201.databinding.ActivityStudyDetailBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class StudyDetail : BindingActivity<ActivityStudyDetailBinding>(R.layout.activity_study_detail) {
+    private val viewModel: StudyDetailViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        initViewModel()
+        initActionBar()
+        initStudyParticipantsList()
+    }
+
+    private fun initStudyParticipantsList() {
+        val studyPeopleAdapter = StudyParticipantsAdapter()
+        binding.rvStudyDetailStudyPeople.setHasFixedSize(true)
+        binding.rvStudyDetailStudyPeople.adapter = studyPeopleAdapter
+        studyPeopleAdapter.submitList(viewModel.studyParticipants.value)
+    }
+
+    private fun initViewModel() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
+        viewModel.getStudyInformation()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.tbStudyDetailAppBar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
