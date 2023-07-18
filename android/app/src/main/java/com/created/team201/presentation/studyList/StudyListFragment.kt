94f75f6c52f7bec1c980a10b80211e7bc7240253
Yerin <myheaven_b@naver.com>
2023-07-19 03:10:01 +0900

    refactor: 스터디 상태에 따른 분기처리 및 뷰 로직 개선

--- android/app/src/main/java/com/created/team201/presentation/studyList/StudyListFragment.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyList/StudyListFragment.kt
@@ -2,13 +2,45 @@ package com.created.team201.presentation.studyList
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.databinding.BindingAdapter
 import com.created.team201.R
 import com.created.team201.databinding.FragmentStudyListBinding
 import com.created.team201.presentation.common.BindingFragment
+import com.created.team201.presentation.studyList.model.StudyStatus
 
 class StudyListFragment : BindingFragment<FragmentStudyListBinding>(R.layout.fragment_study_list) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("isOver")
+        fun isOver(textView: TextView, status: StudyStatus) {
+            textView.setTextColor(
+                when (status) {
+                    StudyStatus.END -> ContextCompat.getColor(
+                        textView.context,
+                        R.color.grey02_78808B,
+                    )
+
+                    else -> ContextCompat.getColor(textView.context, R.color.white)
+                },
+            )
+        }
+
+        @JvmStatic
+        @BindingAdapter("isRecruiting")
+        fun isRecruiting(textView: TextView, status: StudyStatus) {
+            textView.setText(
+                when (status) {
+                    StudyStatus.RECRUITING -> R.string.study_list_processing_date
+                    else -> R.string.study_list_previous_date
+                },
+            )
+        }
     }
 }
--- android/app/src/main/java/com/created/team201/presentation/studyList/model/StudyStatus.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyList/model/StudyStatus.kt
@@ -2,10 +2,10 @@ package com.created.team201.presentation.studyList.model
 
 import com.created.team201.R
 
-enum class StudyStatus(private val id: Int, private val resId: Int, private val strId: Int) {
-    RECRUITING(0, R.drawable.ic_gathering, R.string.study_list_previous_date),
-    PROCESSING(1, R.drawable.ic_processing, R.string.study_list_processing_date),
-    END(2, R.drawable.ic_over, R.string.study_list_processing_date),
+enum class StudyStatus(private val id: Int, private val resId: Int) {
+    RECRUITING(0, R.drawable.ic_gathering),
+    PROCESSING(1, R.drawable.ic_processing),
+    END(2, R.drawable.ic_over),
     ;
 
     companion object {
@@ -15,13 +15,7 @@ enum class StudyStatus(private val id: Int, private val resId: Int, private val 
             }?.resId ?: throw IllegalAccessException("해당 스터디 상태를 찾을 수 없습니다.")
         }
 
-        fun dateFormatOf(id: Int): Int {
-            return StudyStatus.values().find { dateFormat ->
-                dateFormat.id == id
-            }?.strId ?: throw IllegalAccessException("해당 스터디 상태를 찾을 수 없습니다.")
-        }
-
-        fun of(id: Int): StudyStatus {
+        fun valueOf(id: Int): StudyStatus {
             return StudyStatus.values().find { status ->
                 status.id == id
             } ?: throw IllegalAccessException("해당 스터디를 찾을 수 없습니다.")
