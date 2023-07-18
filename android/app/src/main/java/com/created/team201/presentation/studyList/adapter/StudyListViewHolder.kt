5db7ae54bcf9abea384b296f9836bb2fbc9b19f4
Yerin <myheaven_b@naver.com>
2023-07-19 02:14:48 +0900

    feat: 스터디 상태 별 목록에서 보여주는 타이틀 색상 변경

--- android/app/src/main/java/com/created/team201/presentation/studyList/adapter/StudyListViewHolder.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyList/adapter/StudyListViewHolder.kt
@@ -2,10 +2,13 @@ package com.created.team201.presentation.studyList.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.getColor
 import androidx.recyclerview.widget.RecyclerView
 import com.created.team201.R
 import com.created.team201.databinding.ItemStudyListBinding
 import com.created.team201.presentation.studyList.StudyListClickListener
+import com.created.team201.presentation.studyList.model.StudyStatus
+import com.created.team201.presentation.studyList.model.StudyStatus.END
 import com.created.team201.presentation.studyList.model.StudySummaryUiModel
 
 class StudyListViewHolder(
@@ -27,5 +30,10 @@ class StudyListViewHolder(
     fun bind(item: StudySummaryUiModel) {
         studySummary = item
         binding.studySummary = studySummary
+        if (StudyStatus.of(studySummary.processingStatus) == END) {
+            binding.tvStudyListTitle.setTextColor(
+                getColor(parent.context, R.color.grey02_78808B),
+            )
+        }
     }
 }
