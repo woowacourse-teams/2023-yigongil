270fcbb0463618d68866cfe9c846766ef3d3c440
Yerin <myheaven_b@naver.com>
2023-07-20 23:40:21 +0900

    feat: 서버 통신하는 StudyManageService api 작성

--- android/app/src/main/java/com/created/team201/data/remote/NetworkServiceModule.kt
+++ android/app/src/main/java/com/created/team201/data/remote/NetworkServiceModule.kt
@@ -2,9 +2,11 @@ package com.created.team201.data.remote
 
 import com.created.team201.data.remote.api.CreateStudyService
 import com.created.team201.data.remote.api.StudyListService
+import com.created.team201.data.remote.api.StudyManageService
 
 object NetworkServiceModule {
 
     val studyListService = NetworkModule.create<StudyListService>()
     val createStudyService = NetworkModule.create<CreateStudyService>()
+    val studyManageService = NetworkModule.create<StudyManageService>()
 }
