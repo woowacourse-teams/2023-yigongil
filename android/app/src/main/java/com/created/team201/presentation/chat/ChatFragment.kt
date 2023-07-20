721c301fc4b4d9e9f74df460a3139f8cffc3b2c2
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-20 20:33:27 +0900

    chore: 린트체크

--- android/app/src/main/java/com/created/team201/presentation/chat/ChatFragment.kt
+++ android/app/src/main/java/com/created/team201/presentation/chat/ChatFragment.kt
@@ -1,11 +1,7 @@
 package com.created.team201.presentation.chat
 
-import android.os.Bundle
-import android.view.View
 import com.created.team201.R
 import com.created.team201.databinding.FragmentChatBinding
 import com.created.team201.presentation.common.BindingFragment
 
-class ChatFragment : BindingFragment<FragmentChatBinding>(R.layout.fragment_chat) {
-
-}
+class ChatFragment : BindingFragment<FragmentChatBinding>(R.layout.fragment_chat)
