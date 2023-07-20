d7dc718c774e156fa8d5b6bd6eec3ebfe4599672
Yerin <myheaven_b@naver.com>
2023-07-20 10:16:26 +0900

    feat: Study 목록 조회를 하는 StudyListRepository 구현체 생성

---
+++ android/app/src/main/java/com/created/team201/data/repository/StudyListRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package com.created.team201.data.repository
+
+import com.created.domain.StudySummary
+import com.created.domain.repository.StudyListRepository
+import com.created.team201.data.datasource.remote.StudyListDataSource
+import com.created.team201.data.mapper.toDomain
+
+class StudyListRepositoryImpl(
+    private val studyListDataSource: StudyListDataSource,
+) : StudyListRepository {
+
+    override suspend fun getStudyList(): List<StudySummary> {
+        return studyListDataSource.getStudyList().toDomain()
+    }
+}
