037013bd44b4a9b9c559c6b77a90fa7345bce84a
SeHun <81347125+s9hn@users.noreply.github.com>
2023-07-26 14:04:25 +0900

    feat: 로그인뷰 디자인 완료

---
+++ android/app/src/main/java/com/created/team201/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.created.team201.presentation.login
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityLoginBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
