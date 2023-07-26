34bc647c46b250ec13fef73002176729e307a147
Bandal <no1msh1217@gmail.com>
2023-07-26 12:55:48 +0900

    feat: 스터디 관리하기 뷰 구현

---
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementActivity.kt
@@ -0,0 +1,13 @@
+package com.created.team201.presentation.studyManagement
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityStudyManagementBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class StudyManagementActivity :
+    BindingActivity<ActivityStudyManagementBinding>(R.layout.activity_study_management) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
