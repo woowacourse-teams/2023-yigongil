4cdc35b43347cfa3de2e2b155b72fcda020215d0
Bandal <no1msh1217@gmail.com>
2023-07-26 13:49:39 +0900

    feat: toolbar 구현

--- android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementActivity.kt
@@ -1,6 +1,7 @@
 package com.created.team201.presentation.studyManagement
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.created.team201.R
 import com.created.team201.databinding.ActivityStudyManagementBinding
 import com.created.team201.presentation.common.BindingActivity
@@ -9,5 +10,20 @@ class StudyManagementActivity :
     BindingActivity<ActivityStudyManagementBinding>(R.layout.activity_study_management) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.tbStudyManagement)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
