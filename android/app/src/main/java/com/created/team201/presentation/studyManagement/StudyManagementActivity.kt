1ac089568d1a29a6214f94aa3ea695e8bf75ad8d
Yerin <myheaven_b@naver.com>
2023-07-28 00:57:02 +0900

    feat: 스터디 관리하기 뷰 내의 클릭리스너 구현

--- android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import com.created.team201.R
 import com.created.team201.databinding.ActivityStudyManagementBinding
 import com.created.team201.presentation.common.BindingActivity
+import com.created.team201.presentation.common.TodoClickListener
 
 class StudyManagementActivity :
     BindingActivity<ActivityStudyManagementBinding>(R.layout.activity_study_management) {
@@ -30,8 +31,14 @@ class StudyManagementActivity :
     private fun initStudyRounds() {
         val studyId = intent.getLongExtra(KEY_STUDY_ID, KEY_ERROR)
         val roundId = intent.getLongExtra(KEY_ROUND_ID, KEY_ERROR)
-        studyManagementViewModel.getRounds(studyId)
         studyManagementViewModel.getStudyRounds(studyId, roundId)
+    }
+
+    private val todoClickListener = object : TodoClickListener {
+        override fun clickOnTodo(id: Long, isDone: Boolean) {
+            val currentItemId = binding.vpStudyManagement.currentItem
+            studyManagementViewModel.updateTodo(currentItemId, id, !isDone)
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementViewModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.created.domain.model.Round
+import com.created.team201.presentation.home.model.TodoUiModel
 import com.created.team201.presentation.studyManagement.model.StudyRoundDetailUiModel
 
 class StudyManagementViewModel : ViewModel() {
@@ -12,16 +13,71 @@ class StudyManagementViewModel : ViewModel() {
     val studyRounds: LiveData<List<StudyRoundDetailUiModel>>
         get() = _studyRounds
 
-    fun getRounds(studyId: Long) {
-        rounds.value = listOf()
+    fun getStudyRounds(studyId: Long, currentRoundId: Long) {
+        getRounds(studyId)
+        // getStudyRoundDetail() 여러번
+        // _studyRounds.value = listOf()
     }
 
-    fun getStudyRounds(studyId: Long, currentRoundId: Long) {
-        // getStudyRoundDetail()
-        // _studyRounds.value = listOf()
+    private fun getRounds(studyId: Long) {
+        rounds.value = listOf()
     }
 
     private fun getStudyRoundDetail(studyId: Long, roundId: Long) {
         // 서버 통신으로 round 정보 가져옴
+    }
+
+    fun updateTodo(currentItemId: Int, todoId: Long, isDone: Boolean) {
+        val studyDetails = studyRounds.value ?: throw IllegalStateException()
+        val currentStudy = studyRounds.value?.get(currentItemId) ?: throw IllegalStateException()
+        val isNecessary = currentStudy.necessaryTodo.todoId == todoId
+        val study: StudyRoundDetailUiModel
+        val todo: TodoUiModel
+        when (isNecessary) {
+            true -> {
+                updateNecessaryTodoCheck(studyDetails, todoId, isDone)
+                study = studyDetails.find { it.necessaryTodo.todoId == todoId }!!
+                todo = study.necessaryTodo
+            }
+
+            false -> {
+                updateOptionalTodoCheck(studyDetails, todoId, isDone)
+                study = studyDetails.find { it.optionalTodos.any { it.todoId == todoId } }!!
+                todo = study.optionalTodos.find { it.todoId == todoId }!!
+            }
+        }
+
+        patchTodo(todo, study, isNecessary)
+    }
+
+    private fun patchTodo(todo: TodoUiModel, study: StudyRoundDetailUiModel, isNecessary: Boolean) {
+        // 서버 통신으로 update
+    }
+
+    private fun updateNecessaryTodoCheck(
+        studyDetails: List<StudyRoundDetailUiModel>,
+        id: Long,
+        isDone: Boolean,
+    ) {
+        _studyRounds.value = studyDetails.map { studyDetailUiModel ->
+            studyDetailUiModel.takeIf { it.necessaryTodo.todoId != id } ?: studyDetailUiModel.copy(
+                necessaryTodo = studyDetailUiModel.necessaryTodo.copy(isDone = isDone),
+            )
+        }
+    }
+
+    private fun updateOptionalTodoCheck(
+        studyDetails: List<StudyRoundDetailUiModel>,
+        id: Long,
+        isDone: Boolean,
+    ) {
+        _studyRounds.value = studyDetails.map { studyDetailUiModel ->
+            studyDetailUiModel.takeIf { todoUiModel -> !todoUiModel.optionalTodos.any { it.todoId == id } }
+                ?: studyDetailUiModel.copy(
+                    optionalTodos = studyDetailUiModel.optionalTodos.map {
+                        it.takeUnless { it.todoId == id } ?: it.copy(isDone = isDone)
+                    },
+                )
+        }
     }
 }
