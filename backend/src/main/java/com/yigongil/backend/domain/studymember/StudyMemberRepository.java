085a4dea0096fb9745af0cc28684c3723107fef9
bjk1649 <qhwns1649@naver.com>
2023-07-27 17:09:23 +0900

    refactor: @Query 사용한 메서드 파라미터에 Param 어노테이션 추가

--- backend/src/main/java/com/yigongil/backend/domain/studymember/StudyMemberRepository.java
+++ backend/src/main/java/com/yigongil/backend/domain/studymember/StudyMemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.repository.query.Param;
 
 public interface StudyMemberRepository extends Repository<StudyMember, Long> {
 
@@ -28,7 +29,7 @@ public interface StudyMemberRepository extends Repository<StudyMember, Long> {
                 or sm.role = 'STUDY_MEMBER')
                 and sm.studyResult = 'NONE'
             """)
-    List<StudyMember> findAllByStudyIdAndParticipatingAndNotEnd(Long studyId);
+    List<StudyMember> findAllByStudyIdAndParticipatingAndNotEnd(@Param("studyId") Long studyId);
 
     boolean existsByStudyIdAndMemberId(Long studyId, Long memberId);
 
@@ -42,7 +43,7 @@ public interface StudyMemberRepository extends Repository<StudyMember, Long> {
             or sm.role = 'STUDY_MEMBER')
             and sm.studyResult = 'NONE'
             """)
-    List<StudyMember> findAllByMemberIdAndParticipatingAndNotEnd(Long memberId);
+    List<StudyMember> findAllByMemberIdAndParticipatingAndNotEnd(@Param("memberId") Long memberId);
 
     Long countByMemberIdAndStudyResult(Long memberId, StudyResult studyResult);
 }
