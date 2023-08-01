63127bffb45e48354d0b1d9558480450a0e9f9b6
Bandal <no1msh1217@gmail.com>
2023-08-01 18:55:14 +0900

    refactor: id 값 타입 Long으로 통일

--- android/app/src/main/java/com/created/team201/data/remote/api/ProfileService.kt
+++ android/app/src/main/java/com/created/team201/data/remote/api/ProfileService.kt
@@ -1,5 +1,6 @@
 package com.created.team201.data.remote.api
 
+import com.created.team201.data.remote.response.ProfileResponseDto
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -7,5 +8,5 @@ interface ProfileService {
     @GET("/v1/members/{userId}")
     suspend fun getProfile(
         @Path("userId") userId: Long,
-    )
+    ): ProfileResponseDto
 }
--- android/domain/src/main/java/com/created/domain/model/Profile.kt
+++ android/domain/src/main/java/com/created/domain/model/Profile.kt
@@ -2,7 +2,7 @@ package com.created.domain.model
 
 data class Profile(
     val githubId: String,
-    val id: Int,
+    val id: Long,
     val introduction: String?,
     val nickname: String,
     val profileImageUrl: String?,
