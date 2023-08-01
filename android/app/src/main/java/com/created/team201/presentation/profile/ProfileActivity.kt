f6c91882b7ed14d26336dcd5dd0997b732ee2b93
Bandal <no1msh1217@gmail.com>
2023-08-01 20:12:39 +0900

    feat: 종료된 스터디목록 구현

--- android/app/src/main/java/com/created/team201/presentation/profile/ProfileActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/profile/ProfileActivity.kt
@@ -3,26 +3,52 @@ package com.created.team201.presentation.profile
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import com.created.team201.R
 import com.created.team201.databinding.ActivityProfileBinding
 import com.created.team201.presentation.common.BindingActivity
+import com.created.team201.presentation.profile.adapter.FinishedStudyAdapter
 
 class ProfileActivity : BindingActivity<ActivityProfileBinding>(R.layout.activity_profile) {
 
     private val profileViewModel: ProfileViewModel by viewModels { ProfileViewModel.Factory }
     private val userId: Long by lazy { intent.getLongExtra(KEY_USER_ID, DEFAULT_USER_ID) }
+    private val finishedStudyAdapter: FinishedStudyAdapter by lazy { FinishedStudyAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initBinding()
         profileViewModel.initProfile(userId)
+        initFinishedStudyAdapter()
+        submitFinishedStudies()
     }
 
     private fun initBinding() {
         binding.viewModel = profileViewModel
         binding.lifecycleOwner = this
+    }
+
+    private fun initFinishedStudyAdapter() {
+        binding.rvProfileEndedStudies.adapter = finishedStudyAdapter
+        binding.rvProfileEndedStudies.setHasFixedSize(true)
+        val dividerItemDecoration = getDividerItemDecoration()
+        binding.rvProfileEndedStudies.addItemDecoration(dividerItemDecoration)
+    }
+
+    private fun getDividerItemDecoration(): DividerItemDecoration {
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayout.VERTICAL)
+        ContextCompat.getDrawable(this, R.drawable.divider_study_list)?.let {
+            dividerItemDecoration.setDrawable(it)
+        }
+        return dividerItemDecoration
+    }
+
+    private fun submitFinishedStudies() {
+        finishedStudyAdapter.submitList(profileViewModel.profile.value?.finishedStudies)
     }
 
     companion object {
--- android/app/src/main/java/com/created/team201/presentation/profile/ProfileViewModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/profile/ProfileViewModel.kt
@@ -9,6 +9,8 @@ import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.created.domain.model.FinishedStudy
+import com.created.domain.model.Period
+import com.created.domain.model.PeriodUnit
 import com.created.domain.model.Profile
 import com.created.domain.model.UserProfile
 import com.created.domain.repository.ProfileRepository
@@ -18,6 +20,7 @@ import com.created.team201.data.repository.ProfileRepositoryImpl
 import com.created.team201.presentation.myPage.model.ProfileUiModel
 import com.created.team201.presentation.profile.model.FinishedStudyUiModel
 import com.created.team201.presentation.profile.model.UserProfileUiModel
+import com.created.team201.presentation.studyList.model.PeriodUiModel
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(
@@ -32,14 +35,11 @@ class ProfileViewModel(
                 profileRepository.getProfile(userId)
             }.onSuccess {
                 _profile.value = it.toUiModel()
-                Log.d("bandalTest", _profile.value.toString())
-            }.onFailure {
-                Log.d("bandalTest", it.message.toString())
             }
         }
     }
 
-    fun UserProfile.toUiModel(): UserProfileUiModel = UserProfileUiModel(
+    private fun UserProfile.toUiModel(): UserProfileUiModel = UserProfileUiModel(
         profile = profile.toUiModel(),
         finishedStudies = finishedStudies.map { it.toUiModel() },
     )
@@ -63,12 +63,59 @@ class ProfileViewModel(
         name = name,
         numberOfCurrentMembers = numberOfCurrentMembers,
         numberOfMaximumMembers = numberOfMaximumMembers,
-        periodOfRound = periodOfRound,
+        periodOfRound = periodOfRound.toUiModel(),
         startAt = startAt,
         totalRoundCount = totalRoundCount,
     )
 
+    private fun Period.toUiModel(): PeriodUiModel = PeriodUiModel(number = date, unit = unit)
+
     companion object {
+
+        private val dummy = UserProfileUiModel(
+            profile = ProfileUiModel(
+                githubId = "no1msh",
+                id = 1L,
+                introduction = "반달입니다.",
+                nickname = "반달",
+                profileImageUrl = "https://lh3.googleusercontent.com/ogw/AGvuzYaeOM6cCEBXHw7jg4Hyx9nsp9LRg4P2JMpoTX7LSw=s64-c-mo",
+                successRate = 80.6,
+                successfulRoundCount = 79,
+                tier = 2,
+                tierProgress = 70,
+            ),
+            finishedStudies = listOf(
+                FinishedStudyUiModel(
+                    id = 1L,
+                    averageTier = 2,
+                    isSucceed = true,
+                    name = "java 스터디",
+                    numberOfCurrentMembers = 3,
+                    numberOfMaximumMembers = 5,
+                    periodOfRound = PeriodUiModel(
+                        number = 2,
+                        unit = PeriodUnit.DAY,
+                    ),
+                    startAt = "2023.07.23",
+                    totalRoundCount = 20,
+                ),
+                FinishedStudyUiModel(
+                    id = 2L,
+                    averageTier = 3,
+                    isSucceed = false,
+                    name = "kotlin 스터디",
+                    numberOfCurrentMembers = 4,
+                    numberOfMaximumMembers = 6,
+                    periodOfRound = PeriodUiModel(
+                        number = 1,
+                        unit = PeriodUnit.WEEK,
+                    ),
+                    startAt = "2023.07.25",
+                    totalRoundCount = 30,
+                ),
+            ),
+        )
+
         val Factory: ViewModelProvider.Factory = viewModelFactory {
             initializer {
                 ProfileViewModel(
