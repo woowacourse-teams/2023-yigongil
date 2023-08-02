2c25da82fa45f33f8218b23e12a98f85b23eb02c
Bandal <no1msh1217@gmail.com>
2023-08-03 02:34:05 +0900

    feat: 스터디 지원자 수락하기 버튼 클릭 리스너 연결

---
+++ android/app/src/main/java/com/created/team201/presentation/studyDetail/StudyMemberClickListener.kt
@@ -0,0 +1,5 @@
+package com.created.team201.presentation.studyDetail
+
+interface StudyMemberClickListener {
+    fun onAcceptApplicantClick(memberId: Long)
+}
--- android/app/src/main/java/com/created/team201/presentation/studyDetail/adapter/StudyPeopleViewHolder.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyDetail/adapter/StudyPeopleViewHolder.kt
@@ -4,10 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.created.team201.databinding.ItemStudyDetailStudyPeopleBinding
+import com.created.team201.presentation.studyDetail.StudyMemberClickListener
 import com.created.team201.presentation.studyDetail.model.StudyMemberUIModel
 
-class StudyPeopleViewHolder(val binding: ItemStudyDetailStudyPeopleBinding) :
-    RecyclerView.ViewHolder(binding.root) {
+class StudyPeopleViewHolder(
+    val binding: ItemStudyDetailStudyPeopleBinding,
+    studyMemberClickListener: StudyMemberClickListener,
+) : RecyclerView.ViewHolder(binding.root) {
+
+    init {
+        binding.studyMemberClickListener = studyMemberClickListener
+    }
 
     fun bind(studyMemberUIModel: StudyMemberUIModel) {
         binding.studyMember = studyMemberUIModel
