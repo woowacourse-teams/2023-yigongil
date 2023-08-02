4d5ef20164d34a26948b95f995f45fe510b573e7
Bandal <no1msh1217@gmail.com>
2023-08-02 21:16:05 +0900

    feat: Page가 양쪽 끝페이지인지 비교하는 로직 추가

--- android/domain/src/main/java/com/created/domain/model/PageIndex.kt
+++ android/domain/src/main/java/com/created/domain/model/PageIndex.kt
@@ -1,13 +1,17 @@
 package com.created.domain.model
 
 data class PageIndex(
-    val number: Int,
+    val number: Int = 0,
 ) {
     fun increase(maxPage: Int): PageIndex =
         PageIndex((number + ONE_PAGE).coerceAtMost(maxPage))
 
     fun decrease(): PageIndex =
         PageIndex((number - ONE_PAGE).coerceAtLeast(MIN_VIEW_PAGER_INDEX))
+
+    fun isFirstPage(): Boolean = number == MIN_VIEW_PAGER_INDEX
+
+    fun isLastPage(maxPage: Int): Boolean = number == (maxPage - ONE_PAGE)
 
     companion object {
         private const val ONE_PAGE = 1
