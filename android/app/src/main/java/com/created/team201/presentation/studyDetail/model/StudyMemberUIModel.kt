d8204dd4c3301bd1f467385c4b912e579f9db813
Bandal <no1msh1217@gmail.com>
2023-08-03 02:32:39 +0900

    feat: 모델에 id 추가

--- android/app/src/main/java/com/created/team201/presentation/studyDetail/model/StudyMemberUIModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyDetail/model/StudyMemberUIModel.kt
@@ -1,10 +1,23 @@
 package com.created.team201.presentation.studyDetail.model
 
 data class StudyMemberUIModel(
+    val id: Long,
     val isMaster: Boolean,
     val isApplicant: Boolean,
     val profileImageUrl: String,
     val name: String,
     val successRate: Int,
     val tier: Int,
-)
+) {
+    companion object {
+        val DUMMY = StudyMemberUIModel(
+            id = 0L,
+            isMaster = true,
+            isApplicant = false,
+            tier = 3,
+            name = "bandal",
+            successRate = 90,
+            profileImageUrl = "https://opgg-com-image.akamaized.net/attach/images/20200321020018.373875.jpg",
+        )
+    }
+}
