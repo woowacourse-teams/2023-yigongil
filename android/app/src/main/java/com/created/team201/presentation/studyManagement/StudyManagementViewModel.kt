38c6f63603aaab1ab21d51a7a91e7f7066252954
Bandal <no1msh1217@gmail.com>
2023-08-02 21:16:32 +0900

    feat: 스터디 관리하기 뷰의 서버통신 로직 구현

--- android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementViewModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/StudyManagementViewModel.kt
@@ -3,50 +3,117 @@ package com.created.team201.presentation.studyManagement
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.created.domain.model.CreateTodo
 import com.created.domain.model.PageIndex
-import com.created.domain.model.Role
 import com.created.domain.model.Round
+import com.created.domain.model.RoundDetail
+import com.created.domain.model.StudyDetail
+import com.created.domain.model.StudyMember
+import com.created.domain.model.Todo
+import com.created.domain.repository.StudyManagementRepository
+import com.created.team201.data.datasource.remote.StudyManagementDataSourceImpl
+import com.created.team201.data.mapper.toDomain
+import com.created.team201.data.remote.NetworkServiceModule
+import com.created.team201.data.repository.StudyManagementRepositoryImpl
 import com.created.team201.presentation.home.model.TodoUiModel
+import com.created.team201.presentation.studyManagement.adapter.OptionalTodoViewType
+import com.created.team201.presentation.studyManagement.model.OptionalTodoUiModel
+import com.created.team201.presentation.studyManagement.model.RoundUiModel
 import com.created.team201.presentation.studyManagement.model.StudyManagementInformationUiModel
+import com.created.team201.presentation.studyManagement.model.StudyMemberUiModel
 import com.created.team201.presentation.studyManagement.model.StudyRoundDetailUiModel
+import kotlinx.coroutines.launch
 
-class StudyManagementViewModel : ViewModel() {
-    private val rounds: MutableLiveData<List<Round>> = MutableLiveData()
+class StudyManagementViewModel(
+    private val repository: StudyManagementRepository,
+) : ViewModel() {
+    private val rounds: MutableLiveData<List<RoundUiModel>> = MutableLiveData()
+
     private val _studyRounds: MutableLiveData<List<StudyRoundDetailUiModel>> = MutableLiveData()
-    val studyRounds: LiveData<List<StudyRoundDetailUiModel>>
-        get() = _studyRounds
+    val studyRounds: LiveData<List<StudyRoundDetailUiModel>> get() = _studyRounds
+
     private val _state: MutableLiveData<StudyManagementState> =
         MutableLiveData(StudyManagementState.Member)
     val state: LiveData<StudyManagementState> get() = _state
+
     private val _currentRound: MutableLiveData<Int> = MutableLiveData(0)
     val currentRound: LiveData<Int> get() = _currentRound
+
     lateinit var studyInformation: StudyManagementInformationUiModel
 
-    fun fetchStudyInformation() {
-        // dummy
-        studyInformation = StudyManagementInformationUiModel(
-            "자바 스터디", 1, 3, Role.MASTER, "2022.03.04", 3, "3d", 2, "설명입니다.",
-        )
-        // 여기서 서버 호출해서 다 가지고 있게 하자 ~
+    fun fetchStudyInformation(studyId: Long) {
+        viewModelScope.launch {
+            runCatching {
+                repository.getStudyDetail(studyId)
+            }.onSuccess {
+                studyInformation = it.toUiModel()
+                rounds.value = it.rounds.map { round: Round -> round.toUiModel() }
+            }
+        }
     }
 
     fun getStudyRounds(studyId: Long, currentRoundId: Long) {
-        // current Round 초기화
         _currentRound.value = studyInformation.currentRound
-        // to do : studyMaster id,와 비교해서 member에 isMaster 넣어줘야함
-        _studyRounds.value = dummy
-        _state.value = StudyManagementState.Member // Role을 통해 현재 무슨 역할인지 분기처리
-        getRounds(studyId)
-        // getStudyRoundDetail() 여러번
-        // _studyRounds.value = listOf()
+        _state.value = StudyManagementState.Member
+
+        viewModelScope.launch {
+            getStudyRoundDetail(studyId, currentRoundId).onSuccess {
+                _studyRounds.value = listOf(it.toUiModel())
+                fetchRoundDetail(studyId, PageIndex())
+            }
+        }
     }
 
-    private fun getRounds(studyId: Long) {
-        rounds.value = roundDummy
+    fun fetchRoundDetail(studyId: Long, pageIndex: PageIndex) {
+        val studyRounds = studyRounds.value ?: listOf()
+        val rounds = rounds.value ?: listOf()
+        updateCurrentPage(pageIndex)
+
+        if (checkLimitPage()) return
+
+        val index = rounds.indexOf(rounds.find { it.number == currentRound.value })
+
+        viewModelScope.launch {
+            if (pageIndex.isFirstPage()) {
+                getFirstPage(studyId, rounds[index - ONE_PAGE].id, studyRounds)
+            }
+
+            if (pageIndex.isLastPage(studyRounds.size)) {
+                getLastPage(studyId, rounds[index + ONE_PAGE].id, studyRounds)
+            }
+        }
     }
 
-    private fun getStudyRoundDetail(studyId: Long, roundId: Long) {
-        // 서버 통신으로 round 정보 가져옴
+    private suspend fun getFirstPage(
+        studyId: Long,
+        roundId: Long,
+        studyRounds: List<StudyRoundDetailUiModel>,
+    ) {
+        viewModelScope.launch {
+            getStudyRoundDetail(studyId, roundId).onSuccess {
+                _studyRounds.value = listOf(it.toUiModel()) + studyRounds
+            }
+        }
+    }
+
+    private suspend fun getLastPage(
+        studyId: Long,
+        roundId: Long,
+        studyRounds: List<StudyRoundDetailUiModel>,
+    ) {
+        viewModelScope.launch {
+            getStudyRoundDetail(studyId, roundId).onSuccess {
+                _studyRounds.value = studyRounds + it.toUiModel()
+            }
+        }
+    }
+
+    private fun checkLimitPage(): Boolean {
+        if (currentRound.value == rounds.value?.last()?.number) return true
+        if (currentRound.value == rounds.value?.first()?.number) return true
+        return false
     }
 
     fun updateCurrentPage(pageIndex: PageIndex) {
@@ -60,16 +127,13 @@ class StudyManagementViewModel : ViewModel() {
         _currentRound.value = round.number
     }
 
-    fun fetchRoundDetail(pageIndex: PageIndex) {
-        // currentRoundId 갱신
-        updateCurrentPage(pageIndex)
-        // getStudyRoundDetail
-        // 서버 통신
-        // 인덱스 0일때는 앞에거...
-        // 인덱스 마지막일때는.. 뒤에거..
+    private suspend fun getStudyRoundDetail(studyId: Long, roundId: Long): Result<RoundDetail> {
+        return runCatching {
+            repository.getRoundDetail(studyId, roundId)
+        }
     }
 
-    fun updateTodo(currentItemId: Int, todoId: Long, isDone: Boolean) {
+    fun updateTodo(currentItemId: Int, todoId: Long, isDone: Boolean, studyId: Long) {
         val studyDetails = studyRounds.value ?: listOf()
         val isNecessary = studyRounds.value?.get(currentItemId)?.necessaryTodo?.todoId == todoId
         val study: StudyRoundDetailUiModel
@@ -88,11 +152,23 @@ class StudyManagementViewModel : ViewModel() {
             }
         }
 
-        patchTodo(todo, study, isNecessary)
+        patchTodo(todo, isNecessary, studyId)
     }
 
-    private fun patchTodo(todo: TodoUiModel, study: StudyRoundDetailUiModel, isNecessary: Boolean) {
-        // 서버 통신으로 update
+    private fun patchTodo(
+        todo: TodoUiModel,
+        isNecessary: Boolean,
+        studyId: Long,
+    ) {
+        viewModelScope.launch {
+            runCatching {
+                repository.patchTodo(
+                    studyId = studyId,
+                    todo = todo.toDomain(),
+                    isNecessary = isNecessary,
+                )
+            }
+        }
     }
 
     private fun updateNecessaryTodoCheck(
@@ -124,10 +200,101 @@ class StudyManagementViewModel : ViewModel() {
     }
 
     fun addOptionalTodo(studyId: Long, currentPage: Int, todoContent: String) {
-        val currentStudy = studyRounds.value?.get(currentPage)
+        val currentStudyRounds = studyRounds.value ?: listOf()
+        val currentStudy = currentStudyRounds[currentPage]
 
-        // 서버 통신 -> 투두 추가
+        viewModelScope.launch {
+            runCatching {
+                repository.createTodo(studyId, CreateTodo(false, currentStudy.id, todoContent))
+            }.onSuccess { result ->
+                val todoId = result.getOrDefault(DEFAULT_TODO_ID)
+                val newOptionalTodos = getNewOptionalTodos(currentStudy, todoId, todoContent)
 
-        // 서버 통신 -> 다시 받아서 갈아끼워줘야함
+                val newStudy = currentStudy.copy(optionalTodos = newOptionalTodos)
+                val updatedStudyRounds = currentStudyRounds.map { studyRoundDetailUiModel ->
+                    studyRoundDetailUiModel.takeIf { it.id != studyId } ?: newStudy
+                }
+                _studyRounds.value = updatedStudyRounds
+            }
+        }
+    }
+
+    private fun getNewOptionalTodos(
+        currentStudy: StudyRoundDetailUiModel,
+        todoId: Long,
+        todoContent: String,
+    ): MutableList<OptionalTodoUiModel> {
+        val newOptionalTodos = currentStudy.optionalTodos.toMutableList()
+        newOptionalTodos.removeLast()
+        newOptionalTodos.add(
+            OptionalTodoUiModel(
+                TodoUiModel(todoId, todoContent, false),
+                OptionalTodoViewType.DISPLAY.viewType,
+            ),
+        )
+        return newOptionalTodos
+    }
+
+    private fun StudyDetail.toUiModel(): StudyManagementInformationUiModel =
+        StudyManagementInformationUiModel(
+            name = name,
+            numberOfCurrentMembers = numberOfCurrentMembers,
+            numberOfMaximumMembers = numberOfMaximumMembers,
+            role = role,
+            startAt = startAt,
+            totalRoundCount = totalRoundCount,
+            periodOfRound = periodOfRound,
+            currentRound = currentRound,
+            introduction = introduction,
+            rounds = rounds.map { it.toUiModel() },
+        )
+
+    private fun RoundDetail.toUiModel(): StudyRoundDetailUiModel = StudyRoundDetailUiModel(
+        id = id,
+        masterId = masterId,
+        role = role,
+        necessaryTodo = necessaryTodo.toUiModel(),
+        optionalTodos = optionalTodos.map { it.toOptionalTodoUiModel() },
+        studyMembers = members.map { it.toUiModel(masterId) },
+
+    )
+
+    private fun Todo.toUiModel(): TodoUiModel = TodoUiModel(
+        todoId = todoId,
+        content = content,
+        isDone = isDone,
+    )
+
+    private fun StudyMember.toUiModel(masterId: Long): StudyMemberUiModel = StudyMemberUiModel(
+        id = id,
+        isMaster = id == masterId,
+        nickname = nickname,
+        profileImageUrl = profileImageUrl,
+        isDone = isDone,
+
+    )
+
+    private fun Round.toUiModel(): RoundUiModel = RoundUiModel(id = id, number = number)
+
+    private fun Todo.toOptionalTodoUiModel(): OptionalTodoUiModel = OptionalTodoUiModel(
+        todo = this.toUiModel(),
+        viewType = OptionalTodoViewType.DISPLAY.viewType,
+    )
+
+    companion object {
+        private const val DEFAULT_TODO_ID = 0L
+        private const val ONE_PAGE = 1
+
+        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val repository = StudyManagementRepositoryImpl(
+                    StudyManagementDataSourceImpl(
+                        NetworkServiceModule.studyManagementService,
+                    ),
+                )
+                return StudyManagementViewModel(repository) as T
+            }
+        }
     }
 }
