a8850c7fc75c35a4e5ed4a327e4a050b8abbe5db
Jae Min Yu <woawoa0513@gmail.com>
2023-08-04 13:18:12 +0900

    refactor: 로그 내용에 stack trace 포함 (#199)

--- backend/src/main/java/com/yigongil/backend/ui/ApiExceptionHandler.java
+++ backend/src/main/java/com/yigongil/backend/ui/ApiExceptionHandler.java
@@ -14,21 +14,21 @@ public class ApiExceptionHandler {
 
     @ExceptionHandler(HttpException.class)
     public ResponseEntity<String> handleHttpException(HttpException e) {
-        log.error("예외 발생: " + e);
+        log.error("예외 발생: ", e);
         return ResponseEntity.status(e.getHttpStatus())
                              .body(e.getMessageWithInput());
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
     public ResponseEntity<String> handleExpiredJwt(ExpiredJwtException e) {
-        log.info("토큰 만료: " + e);
+        log.info("토큰 만료: ", e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                              .body("토큰이 만료되었습니다");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
-        log.error("예상치 못한 예외 발생: " + e);
+        log.error("예상치 못한 예외 발생: ", e);
         return ResponseEntity.internalServerError()
                              .body("서버 에러 발생");
     }
