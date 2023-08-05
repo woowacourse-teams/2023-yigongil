2688b454515c9b104c1a143a29d802a5991454ed
kevstevie <jianna442@gmail.com>
2023-08-05 18:28:08 +0900

    feat: api의 쿼리르 카운트하는 aop

--- backend/src/main/java/com/yigongil/backend/config/WebConfig.java
+++ backend/src/main/java/com/yigongil/backend/config/WebConfig.java
@@ -13,27 +13,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     private final MemberArgumentResolver memberArgumentResolver;
-    private final LoginInterceptor loginInterceptor;
+    private final AuthInterceptor authInterceptor;
     private final LoggingInterceptor loggingInterceptor;
 
-    public WebConfig(
-            MemberArgumentResolver memberArgumentResolver,
-            LoginInterceptor loginInterceptor,
-            LoggingInterceptor loggingInterceptor
-    ) {
+    public WebConfig(MemberArgumentResolver memberArgumentResolver, AuthInterceptor authInterceptor, LoggingInterceptor loggingInterceptor) {
         this.memberArgumentResolver = memberArgumentResolver;
-        this.loginInterceptor = loginInterceptor;
+        this.authInterceptor = authInterceptor;
         this.loggingInterceptor = loggingInterceptor;
     }
 
     @Override
-    public void addInterceptors(final InterceptorRegistry registry) {
+    public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/v1/**")
                 .excludePathPatterns("/v1/login/**")
                 .excludePathPatterns("/v1/members/{id:[0-9]\\d*}")
-                .excludePathPatterns("/v1/studies/recruiting");
-      
+                .excludePathPatterns("/v1/studies/recruiting")
+                .order(2);
+
         registry.addInterceptor(loggingInterceptor)
                 .addPathPatterns("/**")
                 .order(3);
