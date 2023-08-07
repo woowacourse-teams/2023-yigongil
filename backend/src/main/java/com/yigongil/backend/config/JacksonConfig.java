f35e62980a5e4ce76d170af288149aafb46f3eab
yujamint <woawoa0513@gmail.com>
2023-08-07 23:45:04 +0900

    style: 일관성을 위한 클래스명 수정

--- backend/src/main/java/com/yigongil/backend/config/JacksonConfig.java
+++ backend/src/main/java/com/yigongil/backend/config/JacksonConfig.java
@@ -15,7 +15,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class JacksonConfiguration {
+public class JacksonConfig {
 
     @Bean
     public ObjectMapper serializingObjectMapper() {
