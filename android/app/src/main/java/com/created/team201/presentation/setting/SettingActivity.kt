11d310482aa8eec980c4210b1d3a40f443441889
inseonyun <yuninseon@naver.com>
2023-08-08 00:18:58 +0900

    feat: 설정 뷰 작성

---
+++ android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
@@ -0,0 +1,13 @@
+package com.created.team201.presentation.setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivitySettingBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
