3ae9e754addbf0c01ebfef293c19f91901362a84
Yerin <myheaven_b@naver.com>
2023-08-08 15:12:01 +0900

    feat: 신고하기 뷰 작성

---
+++ android/app/src/main/java/com/created/team201/presentation/report/ReportActivity.kt
@@ -0,0 +1,13 @@
+package com.created.team201.presentation.report
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityReportBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class ReportActivity : BindingActivity<ActivityReportBinding>(R.layout.activity_report) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
