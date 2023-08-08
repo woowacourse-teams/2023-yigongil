4a337412a31f3e95e15db34ea3c3be7a67d47a1a
inseonyun <yuninseon@naver.com>
2023-08-08 10:16:31 +0900

    feat: 계정 관리 뷰 작성

--- android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
@@ -1,6 +1,5 @@
 package com.created.team201.presentation.setting
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.created.team201.R
 import com.created.team201.databinding.ActivitySettingBinding
