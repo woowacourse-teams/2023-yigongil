e84f6029f5909784abb2ded2add9b202a5e97b1e
inseonyun <yuninseon@naver.com>
2023-08-08 19:39:14 +0900

    feat: Setting 기능 구현

--- android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
@@ -6,13 +6,40 @@ import android.os.Bundle
 import android.view.MenuItem
 import com.created.team201.R
 import com.created.team201.databinding.ActivitySettingBinding
+import com.created.team201.presentation.accountSetting.AccountSettingActivity
 import com.created.team201.presentation.common.BindingActivity
+import com.created.team201.presentation.setting.adapter.SettingAdapter
+import com.created.team201.presentation.setting.model.SettingType
+import com.created.team201.presentation.setting.model.SettingUiModel
 
 class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+    private val settingItems: List<SettingUiModel> by lazy {
+        resources.getStringArray(R.array.settingItems).mapIndexed { index, setting ->
+            SettingUiModel(index.toLong(), setting)
+        }
+    }
+
+    private val onSettingItemClick: SettingClickListener by lazy {
+        object : SettingClickListener {
+            override fun onClick(itemId: Long) {
+                when (SettingType.valueOf(itemId)) {
+                    SettingType.NOTIFICATION -> {}
+                    SettingType.ACCOUNT -> {
+                        navigateToAccountSetting()
+                    }
+
+                    SettingType.POLICY -> {}
+                    SettingType.LOGOUT -> {}
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initActionBar()
+        initSettingRecyclerView()
     }
 
     private fun initActionBar() {
@@ -20,6 +47,16 @@ class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activit
         supportActionBar?.setHomeActionContentDescription(R.string.toolbar_back_text)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initSettingRecyclerView() {
+        binding.rvSetting.hasFixedSize()
+        binding.rvSetting.adapter =
+            SettingAdapter(onSettingItemClick).also { it.submitList(settingItems) }
+    }
+
+    private fun navigateToAccountSetting() {
+        startActivity(AccountSettingActivity.getIntent(this))
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean =
@@ -33,6 +70,9 @@ class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activit
         }
 
     companion object {
-        fun getIntent(context: Context): Intent = Intent(context, SettingActivity::class.java)
+        fun getIntent(context: Context): Intent =
+            Intent(context, SettingActivity::class.java).also {
+                it.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            }
     }
 }
