0dad5d565d1c78f4c696c477aa3108b97221b618
inseonyun <yuninseon@naver.com>
2023-08-08 17:06:10 +0900

    feat: MyPage와 Setting 화면 연결 및 툴바 설정

--- android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/setting/SettingActivity.kt
@@ -1,6 +1,9 @@
 package com.created.team201.presentation.setting
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import com.created.team201.R
 import com.created.team201.databinding.ActivitySettingBinding
 import com.created.team201.presentation.common.BindingActivity
@@ -8,5 +11,28 @@ import com.created.team201.presentation.common.BindingActivity
 class SettingActivity : BindingActivity<ActivitySettingBinding>(R.layout.activity_setting) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initActionBar()
+    }
+
+    private fun initActionBar() {
+        setSupportActionBar(binding.tbSetting)
+        supportActionBar?.setHomeActionContentDescription(R.string.toolbar_back_text)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> false
+        }
+
+    companion object {
+        fun getIntent(context: Context): Intent = Intent(context, SettingActivity::class.java)
     }
 }
