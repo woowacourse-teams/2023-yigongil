563379bdb3ed36b7134fbc1817450e6fdc8371d6
Yerin <myheaven_b@naver.com>
2023-08-08 15:13:54 +0900

    feat: 툴바에 뒤로가기 버튼 추가

--- android/app/src/main/java/com/created/team201/presentation/report/ReportActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/report/ReportActivity.kt
@@ -1,6 +1,7 @@
 package com.created.team201.presentation.report
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.created.team201.R
 import com.created.team201.databinding.ActivityReportBinding
 import com.created.team201.presentation.common.BindingActivity
@@ -9,5 +10,29 @@ class ReportActivity : BindingActivity<ActivityReportBinding>(R.layout.activity_
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initToolBar()
+    }
+
+    private fun initToolBar() {
+        setSupportActionBar(binding.tbReport)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_back)
+            setHomeActionContentDescription(R.string.toolbar_back_text)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+
+            else -> {
+                false
+            }
+        }
     }
 }
