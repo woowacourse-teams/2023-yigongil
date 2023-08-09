726076f09f262ffa62736f0891aeaadc4848ac76
inseonyun <yuninseon@naver.com>
2023-08-09 23:58:59 +0900

    feat: 회원 탈퇴 다이얼로그 및 기능 구현

--- android/app/src/main/java/com/created/team201/presentation/accountSetting/AccountSettingActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/accountSetting/AccountSettingActivity.kt
@@ -4,16 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.fragment.app.commit
 import com.created.team201.R
 import com.created.team201.databinding.ActivityAccountSettingBinding
 import com.created.team201.presentation.common.BindingActivity
+import com.created.team201.presentation.login.LoginActivity
+import com.created.team201.presentation.setting.SettingDialog
+import com.created.team201.presentation.setting.SettingDialogClickListener
+import com.created.team201.presentation.setting.SettingViewModel
+import com.created.team201.presentation.setting.SettingViewModel.State.FAIL
+import com.created.team201.presentation.setting.SettingViewModel.State.IDLE
+import com.created.team201.presentation.setting.SettingViewModel.State.SUCCESS
 
 class AccountSettingActivity :
     BindingActivity<ActivityAccountSettingBinding>(R.layout.activity_account_setting) {
+    private val viewModel: SettingViewModel by viewModels {
+        SettingViewModel.Factory
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         initActionBar()
+        initBinding()
+        setWithdrawAccountClick()
+        setObserveWithdrawAccountResult()
     }
 
     private fun initActionBar() {
@@ -21,6 +38,64 @@ class AccountSettingActivity :
         supportActionBar?.setHomeActionContentDescription(R.string.toolbar_back_text)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun setWithdrawAccountClick() {
+        binding.tvAccountSettingWithdrawAccount.setOnClickListener {
+            removeDialog()
+            SettingDialog(
+                getString(R.string.setting_dialog_withdraw_account_title),
+                getString(R.string.setting_dialog_withdraw_account_content),
+                object : SettingDialogClickListener {
+                    override fun onCancelClick() {}
+
+                    override fun onOkClick() {
+                        viewModel.withdrawAccount()
+                    }
+                }
+            ).show(supportFragmentManager, TAG_DIALOG_WITHDRAW_ACCOUNT)
+        }
+    }
+
+    private fun removeDialog() {
+        supportFragmentManager.findFragmentByTag(TAG_DIALOG_WITHDRAW_ACCOUNT)?.let {
+            supportFragmentManager.commit {
+                remove(it)
+            }
+        }
+    }
+
+    private fun setObserveWithdrawAccountResult() {
+        viewModel.isWithdrawAccountState.observe(this) { state ->
+            when (state) {
+                SUCCESS -> {
+                    navigateToLogin()
+                }
+
+                FAIL -> {
+                    showToast(
+                        getString(R.string.setting_dialog_toast_withdraw_account_fail)
+                    )
+                }
+
+                IDLE -> throw IllegalStateException()
+            }
+        }
+    }
+
+    private fun navigateToLogin() {
+        startActivity(
+            LoginActivity.getIntent(this).also { it.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP })
+        finishAffinity()
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean =
@@ -34,6 +109,8 @@ class AccountSettingActivity :
         }
 
     companion object {
+        private const val TAG_DIALOG_WITHDRAW_ACCOUNT = "TAG_DIALOG_WITHDRAW_ACCOUNT"
+
         fun getIntent(context: Context): Intent =
             Intent(context, AccountSettingActivity::class.java).also {
                 it.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
