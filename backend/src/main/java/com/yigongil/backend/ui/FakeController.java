dfa0ab0f35f0e5623080c6a777bba427765a3c76
dhkim <akdlzmf1123@gmail.com>
2023-08-10 14:44:21 +0900

    refactor: 스터디 진행 관련 테스트 fake 테스트 방식으로 변경

--- backend/src/main/java/com/yigongil/backend/ui/FakeController.java
+++ backend/src/main/java/com/yigongil/backend/ui/FakeController.java
@@ -1,15 +1,18 @@
 package com.yigongil.backend.ui;
 
+import com.yigongil.backend.application.StudyService;
 import com.yigongil.backend.config.oauth.JwtTokenProvider;
 import com.yigongil.backend.domain.member.Member;
 import com.yigongil.backend.domain.member.MemberRepository;
 import com.yigongil.backend.response.TokenResponse;
+import java.time.LocalDate;
 import java.util.Optional;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,10 +22,12 @@ public class FakeController {
 
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final StudyService studyService;
 
-    public FakeController(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider) {
+    public FakeController(MemberRepository memberRepository, JwtTokenProvider jwtTokenProvider, StudyService studyService) {
         this.memberRepository = memberRepository;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.studyService = studyService;
     }
 
     @GetMapping("/v1/login/fake/tokens")
@@ -38,5 +43,11 @@ public class FakeController {
                 )
         ).getId();
         return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE).body(new TokenResponse(jwtTokenProvider.createToken(id)));
+    }
+
+    @PutMapping("/v1/fake/proceed")
+    public ResponseEntity<Void> proceed(@RequestParam Integer days) {
+        studyService.proceedRound(LocalDate.now().plusDays(days));
+        return ResponseEntity.ok().build();
     }
 }
