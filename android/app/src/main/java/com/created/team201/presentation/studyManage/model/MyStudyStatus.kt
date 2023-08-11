76e5d03522667ecba210c81f65b4fbad38f40b5e
Yerin <myheaven_b@naver.com>
2023-08-11 19:10:21 +0900

    feat: 참여한/개설한 스터디가 없는 경우 안내 문구

--- android/app/src/main/java/com/created/team201/presentation/studyManage/model/MyStudyStatus.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManage/model/MyStudyStatus.kt
@@ -1,5 +1,9 @@
 package com.created.team201.presentation.studyManage.model
 
-enum class MyStudyStatus(val id: Long) {
-    PARTICIPATED(0), OPENED(1)
+import androidx.annotation.StringRes
+import com.created.team201.R
+
+enum class MyStudyStatus(val id: Long, @StringRes val notice: Int) {
+    PARTICIPATED(0, R.string.study_manage_joined_tab_no_study),
+    OPENED(1, R.string.study_manage_opened_tab_no_study),
 }
