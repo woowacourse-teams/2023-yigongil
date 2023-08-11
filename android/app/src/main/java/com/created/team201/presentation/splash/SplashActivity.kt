2117af6588aca7339da53dc3b76ff1ab0e8525ec
SeHun <81347125+s9hn@users.noreply.github.com>
2023-08-11 19:56:53 +0900

    refactor: default splash화면 삭제

--- android/app/src/main/java/com/created/team201/presentation/splash/SplashActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/splash/SplashActivity.kt
@@ -3,7 +3,6 @@ package com.created.team201.presentation.splash
 import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
 import com.created.team201.R
 import com.created.team201.databinding.ActivitySplashBinding
 import com.created.team201.presentation.MainActivity
@@ -13,8 +12,6 @@ import com.created.team201.presentation.onBoarding.model.OnBoardingDoneState
 import com.created.team201.presentation.splash.SplashViewModel.State.FAIL
 import com.created.team201.presentation.splash.SplashViewModel.State.IDLE
 import com.created.team201.presentation.splash.SplashViewModel.State.SUCCESS
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
@@ -25,14 +22,8 @@ class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_
 
         observeLoginState()
         observeOnBoardingDoneState()
-        delayView()
     }
 
-    private fun delayView() {
-        lifecycleScope.launch {
-            delay(800)
-        }
-    }
 
     private fun observeLoginState() {
         splashViewModel.loginState.observe(this@SplashActivity) { loginState ->
