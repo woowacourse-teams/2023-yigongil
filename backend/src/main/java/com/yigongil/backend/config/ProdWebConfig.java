d40da950199432fd4ed5a012894cdf54f70a1e0b
kevstevie <jianna442@gmail.com>
2023-08-12 21:24:29 +0900

    fix: 충돌 수정

--- backend/src/main/java/com/yigongil/backend/config/ProdWebConfig.java
+++ backend/src/main/java/com/yigongil/backend/config/ProdWebConfig.java
@@ -33,7 +33,7 @@ public class ProdWebConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/v1/studies/recruiting/**")
                 .excludePathPatterns("/v1/api/**")
                 .excludePathPatterns("/v1/api-docs/**")
-                .excludePathPatterns("/v1/swagger-ui/**");
+                .excludePathPatterns("/v1/swagger-ui/**")
                 .excludePathPatterns("/v1/actuator/**");
 
     }
