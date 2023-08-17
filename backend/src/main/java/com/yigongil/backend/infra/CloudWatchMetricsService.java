fb892ece0ea0dec8228da4ef82508ca938718cd8
yujamint <woawoa0513@gmail.com>
2023-08-17 15:27:10 +0900

    feat: CloudWatch Actuator tag 수정

--- backend/src/main/java/com/yigongil/backend/infra/CloudWatchMetricsService.java
+++ backend/src/main/java/com/yigongil/backend/infra/CloudWatchMetricsService.java
@@ -6,9 +6,12 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudwatch.CloudWatchClient;
+import software.amazon.awssdk.services.cloudwatch.model.Dimension;
 import software.amazon.awssdk.services.cloudwatch.model.MetricDatum;
 import software.amazon.awssdk.services.cloudwatch.model.PutMetricDataRequest;
 import software.amazon.awssdk.services.cloudwatch.model.StandardUnit;
+
+import java.util.List;
 
 import static org.springframework.boot.actuate.metrics.MetricsEndpoint.MetricResponse;
 import static org.springframework.boot.actuate.metrics.MetricsEndpoint.Sample;
@@ -29,17 +32,28 @@ public class CloudWatchMetricsService {
     @Scheduled(fixedDelay = 60_000)
     public void pushMetricsToCloudWatch() {
         MetricResponse metrics = metricsEndpoint.metric("http.server.requests", null);
+
         for (Sample sample : metrics.getMeasurements()) {
-            MetricDatum datum = MetricDatum.builder()
-                    .metricName(sample.getStatistic().toString())
-                    .value(sample.getValue())
-                    .unit(StandardUnit.COUNT)
-                    .build();
-            PutMetricDataRequest request = PutMetricDataRequest.builder()
-                    .namespace("yigongil-prod")
-                    .metricData(datum)
-                    .build();
-            CLOUD_WATCH_CLIENT.putMetricData(request);
+            List<String> uris = metrics.getAvailableTags().stream()
+                    .filter(tag -> "uri".equals(tag.getTag()))
+                    .flatMap(tag -> tag.getValues().stream())
+                    .toList();
+
+            for (String uri : uris) {
+                MetricDatum datum = MetricDatum.builder()
+                        .metricName("http.server.requests." + sample.getStatistic())
+                        .value(sample.getValue())
+                        .unit(StandardUnit.COUNT)
+                        .dimensions(Dimension.builder().name("URI").value(uri).build())
+                        .build();
+
+                PutMetricDataRequest request = PutMetricDataRequest.builder()
+                        .namespace("yigongil-prod")
+                        .metricData(datum)
+                        .build();
+
+                CLOUD_WATCH_CLIENT.putMetricData(request);
+            }
         }
     }
 }
