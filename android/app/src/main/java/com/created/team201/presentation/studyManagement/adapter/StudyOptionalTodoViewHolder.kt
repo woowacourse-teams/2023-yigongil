cc04cc3c5fbf3a7eea361d5b226f9acfb69900ef
Yerin <myheaven_b@naver.com>
2023-08-18 11:18:34 +0900

    refactor: 최초 투두 생성 후 편집 시 안보이는 현상 수정 및 bind override

--- android/app/src/main/java/com/created/team201/presentation/studyManagement/adapter/StudyOptionalTodoViewHolder.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyManagement/adapter/StudyOptionalTodoViewHolder.kt
@@ -3,8 +3,12 @@ package com.created.team201.presentation.studyManagement.adapter
 import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
 import com.created.team201.presentation.studyManagement.StudyManagementClickListener
+import com.created.team201.presentation.studyManagement.model.OptionalTodoUiModel
 
 abstract class StudyOptionalTodoViewHolder(
     val binding: ViewDataBinding,
     studyManagementClickListener: StudyManagementClickListener,
-) : RecyclerView.ViewHolder(binding.root)
+) : RecyclerView.ViewHolder(binding.root) {
+
+    abstract fun bind(item: OptionalTodoUiModel)
+}
