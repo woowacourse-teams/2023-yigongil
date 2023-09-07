658154d99a20794d5de2135759b0731c914a7b5b
Yerin <myheaven_b@naver.com>
2023-09-07 16:01:46 +0900

    feat: 티어 이미지 변경 및 아이템 재배치

--- android/app/src/main/java/com/created/team201/presentation/studyList/model/TierImage.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyList/model/TierImage.kt
@@ -3,11 +3,11 @@ package com.created.team201.presentation.studyList.model
 import com.created.team201.R
 
 enum class TierImage(private val id: Int, private val resId: Int) {
-    BRONZE(1, R.drawable.ic_bronze_24),
-    SILVER(2, R.drawable.ic_silver_24),
-    GOLD(3, R.drawable.ic_gold_24),
-    PLATINUM(4, R.drawable.ic_platinum_24),
-    DIAMOND(5, R.drawable.ic_diamond_24),
+    BRONZE(1, R.drawable.ic_bronze_chip),
+    SILVER(2, R.drawable.ic_silver_chip),
+    GOLD(3, R.drawable.ic_gold_chip),
+    DIAMOND(4, R.drawable.ic_diamond_chip),
+    MASTER(5, R.drawable.ic_master_chip),
     ;
 
     companion object {
