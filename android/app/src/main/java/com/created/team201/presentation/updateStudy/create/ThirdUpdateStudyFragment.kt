7d95305704b9f9872feb11342f609dbaebb75e83
inseonyun <yuninseon@naver.com>
2023-09-07 16:12:50 +0900

    feat: ThirdUpdateStudyFragment 뷰 작성

---
+++ android/app/src/main/java/com/created/team201/presentation/updateStudy/create/ThirdUpdateStudyFragment.kt
@@ -0,0 +1,8 @@
+package com.created.team201.presentation.updateStudy.create
+
+import com.created.team201.R
+import com.created.team201.databinding.FragmentThirdUpdateStudyBinding
+import com.created.team201.presentation.common.BindingFragment
+
+class ThirdUpdateStudyFragment :
+    BindingFragment<FragmentThirdUpdateStudyBinding>(R.layout.fragment_third_update_study)
