506540ccf2e68de849dbdf4e0927ba0f534f49fc
inseonyun <yuninseon@naver.com>
2023-09-07 16:00:51 +0900

    feat: FirstUpdateStudyFragment 뷰 작성

---
+++ android/app/src/main/java/com/created/team201/presentation/updateStudy/create/FirstUpdateStudyFragment.kt
@@ -0,0 +1,8 @@
+package com.created.team201.presentation.updateStudy.create
+
+import com.created.team201.R
+import com.created.team201.databinding.FragmentFirstUpdateStudyBinding
+import com.created.team201.presentation.common.BindingFragment
+
+class FirstUpdateStudyFragment :
+    BindingFragment<FragmentFirstUpdateStudyBinding>(R.layout.fragment_first_update_study)
