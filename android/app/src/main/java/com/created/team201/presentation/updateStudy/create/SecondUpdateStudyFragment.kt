d96c5e9fedf11b415bbf231d22d6cd95de3e6e4a
inseonyun <yuninseon@naver.com>
2023-09-07 16:05:40 +0900

    feat: SecondUpdateStudyFragment 뷰 작성

---
+++ android/app/src/main/java/com/created/team201/presentation/updateStudy/create/SecondUpdateStudyFragment.kt
@@ -0,0 +1,8 @@
+package com.created.team201.presentation.updateStudy.create
+
+import com.created.team201.R
+import com.created.team201.databinding.FragmentSecondUpdateStudyBinding
+import com.created.team201.presentation.common.BindingFragment
+
+class SecondUpdateStudyFragment :
+    BindingFragment<FragmentSecondUpdateStudyBinding>(R.layout.fragment_second_update_study)
