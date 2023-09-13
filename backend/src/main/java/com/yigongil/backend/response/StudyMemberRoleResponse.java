2f134a37e628deb800203e81dbfb8b32f0ab241e
yujamint <woawoa0513@gmail.com>
2023-09-13 13:17:22 +0900

    style: 개행 추가

--- backend/src/main/java/com/yigongil/backend/response/StudyMemberRoleResponse.java
+++ backend/src/main/java/com/yigongil/backend/response/StudyMemberRoleResponse.java
@@ -3,7 +3,9 @@ package com.yigongil.backend.response;
 import com.yigongil.backend.domain.studymember.Role;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record StudyMemberRoleResponse(@Schema(example = "1") int role) {
+public record StudyMemberRoleResponse(
+        @Schema(example = "1") int role
+) {
 
     public static StudyMemberRoleResponse from(Role role) {
         return new StudyMemberRoleResponse(role.getCode());
