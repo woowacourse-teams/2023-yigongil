22decd993e79b793f0b0d93f2d99185287ccb2bd
SeHun <81347125+s9hn@users.noreply.github.com>
2023-09-20 02:37:33 +0900

    feat: 스레드뷰 뷰작업완료

---
+++ android/app/src/main/java/com/created/team201/presentation/studyThread/ThreadActivity.kt
@@ -0,0 +1,12 @@
+package com.created.team201.presentation.studyThread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.created.team201.R
+
+class ThreadActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_thread)
+    }
+}
