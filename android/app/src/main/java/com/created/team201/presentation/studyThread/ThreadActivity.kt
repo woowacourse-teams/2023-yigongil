d782f295ce3908492d13bad36aacbb0493fe8b0a
SeHun <81347125+s9hn@users.noreply.github.com>
2023-09-20 03:00:08 +0900

    feat: 스레드뷰 리사이클러뷰 구현완료

--- android/app/src/main/java/com/created/team201/presentation/studyThread/ThreadActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyThread/ThreadActivity.kt
@@ -1,12 +1,39 @@
 package com.created.team201.presentation.studyThread
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.created.team201.R
+import com.created.team201.databinding.ActivityThreadBinding
+import com.created.team201.presentation.common.BindingActivity
+import com.created.team201.presentation.studyThread.adapter.MustDoAdapter
+import com.created.team201.presentation.studyThread.adapter.ThreadAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
-class ThreadActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class ThreadActivity : BindingActivity<ActivityThreadBinding>(R.layout.activity_thread) {
+    private val mustDoAdapter by lazy { MustDoAdapter() }
+    private val threadAdapter by lazy { ThreadAdapter() }
+    private val threadViewModel: ThreadViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_thread)
+
+        attachAdapter()
+        bindViewModel()
+        binding.ivThreadBackButton.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun bindViewModel() {
+        binding.lifecycleOwner = this
+        binding.vm = threadViewModel
+    }
+
+    private fun attachAdapter() {
+        binding.rvThread.adapter = threadAdapter
+        binding.rvThread.setHasFixedSize(true)
+        binding.rvMustDo.adapter = mustDoAdapter
+        binding.rvMustDo.setHasFixedSize(true)
     }
 }
---
+++ android/app/src/main/java/com/created/team201/presentation/studyThread/ThreadViewModel.kt
@@ -0,0 +1,9 @@
+package com.created.team201.presentation.studyThread
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class ThreadViewModel : ViewModel() {
+
+}
