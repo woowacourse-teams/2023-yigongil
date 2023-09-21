f6c4240cbabd352fa98bc9026af9bb89a4881d0c
SeHun <81347125+s9hn@users.noreply.github.com>
2023-09-21 14:26:46 +0900

    refactor: 수신객체명 수정

--- android/app/src/main/java/com/created/team201/data/mapper/HomeMapper.kt
+++ android/app/src/main/java/com/created/team201/data/mapper/HomeMapper.kt
@@ -5,25 +5,25 @@ import com.created.team201.data.remote.response.HomeResponseDto
 import com.created.team201.presentation.home.uiState.UserStudyUiState
 
 
-fun HomeResponseDto.toUserStudy(): List<UserStudy> = this.studies.map {
+fun HomeResponseDto.toUserStudy(): List<UserStudy> = this.studies.map { study ->
     UserStudy(
-        isMaster = it.isMaster,
-        grassCount = it.grassCount,
-        id = it.id,
-        leftDays = it.leftDays,
-        name = it.name,
-        todoContent = it.todoContent
+        isMaster = study.isMaster,
+        grassCount = study.grassCount,
+        id = study.id,
+        leftDays = study.leftDays,
+        name = study.name,
+        todoContent = study.todoContent
     )
 }
 
 fun List<UserStudy>.toUserStudyUiState(): List<UserStudyUiState> =
-    this.map {
+    this.map { study ->
         UserStudyUiState(
-            studyId = it.id,
-            isMaster = it.isMaster,
-            studyName = it.name,
-            dDay = it.leftDays,
-            mustDo = it.todoContent,
-            grassSeed = it.grassCount
+            studyId = study.id,
+            isMaster = study.isMaster,
+            studyName = study.name,
+            dDay = study.leftDays,
+            mustDo = study.todoContent,
+            grassSeed = study.grassCount
         )
     }
