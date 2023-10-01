8b3918cf861078f142b03a214474491c6ef671b7
inseonyun <yuninseon@naver.com>
2023-10-01 15:41:21 +0900

    feat: CreateStudyActivity 생성

---
+++ android/app/src/main/java/com/created/team201/presentation/createStudy/CreateStudyActivity.kt
@@ -0,0 +1,14 @@
+package com.created.team201.presentation.createStudy
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityCreateStudyBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class CreateStudyActivity :
+    BindingActivity<ActivityCreateStudyBinding>(R.layout.activity_create_study) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
