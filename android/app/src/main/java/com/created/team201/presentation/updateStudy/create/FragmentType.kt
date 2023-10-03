02fcc4ebf03ef72dd878a8ef7a4b230f07a4fdef
inseonyun <yuninseon@naver.com>
2023-10-04 00:03:22 +0900

    refactor: UpdateStudy관련 파일들 모두 제거

--- android/app/src/main/java/com/created/team201/presentation/updateStudy/create/FragmentType.kt
+++
@@ -1,5 +0,0 @@
-package com.created.team201.presentation.updateStudy.create
-
-enum class FragmentType {
-    FIRST, SECOND, THIRD;
-}
--- android/app/src/main/java/com/created/team201/presentation/updateStudy/create/SecondUpdateStudyFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.created.team201.presentation.updateStudy.create
-
-import com.created.team201.R
-import com.created.team201.databinding.FragmentSecondUpdateStudyBinding
-import com.created.team201.presentation.common.BindingFragment
-
-class SecondUpdateStudyFragment :
-    BindingFragment<FragmentSecondUpdateStudyBinding>(R.layout.fragment_second_update_study)
