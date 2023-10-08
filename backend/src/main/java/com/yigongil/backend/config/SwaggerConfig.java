2c29b6534c928954f4e3391fcc374bb3b25de6ed
kevstevie <jianna442@gmail.com>
2023-10-08 14:12:41 +0900

    refactor: swagger 설정 변경

--- backend/src/main/java/com/yigongil/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/yigongil/backend/config/SwaggerConfig.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityScheme.Type;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,8 +30,12 @@ public class SwaggerConfig {
 
         Components components = new Components().addSecuritySchemes("token", securityScheme);
 
+        Server server = new Server();
+        server.setUrl("https://201-study.shop/v1");
+
         return new OpenAPI()
                 .info(info)
-                .components(components);
+                .components(components)
+                .addServersItem(server);
     }
 }
