f4a41162c18f403bcabf0f25f31a6feb0b420118
Yerin <myheaven_b@naver.com>
2023-10-18 01:53:30 +0900

    feat: CertificationActivity 생성 및 뷰 작업

---
+++ android/app/src/main/java/com/created/team201/presentation/certification/CertificationActivity.kt
@@ -0,0 +1,8 @@
+package com.created.team201.presentation.certification
+
+import com.created.team201.R
+import com.created.team201.databinding.ActivityCertificationBinding
+import com.created.team201.presentation.common.BindingActivity
+
+class CertificationActivity :
+    BindingActivity<ActivityCertificationBinding>(R.layout.activity_certification)
