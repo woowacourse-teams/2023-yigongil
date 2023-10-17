f09c65d3f4de158eded01079a57e5e2fc8ec43de
Yerin <myheaven_b@naver.com>
2023-10-18 03:26:50 +0900

    feat: CertificationCheckActivity 생성 및 뷰 작업

---
+++ android/app/src/main/java/com/created/team201/presentation/certificationCheck/CertificationCheckActivity.kt
@@ -0,0 +1,16 @@
+package com.created.team201.presentation.certificationCheck
+
+import android.os.Bundle
+import com.created.team201.R
+import com.created.team201.databinding.ActivityCertificationCheckBinding
+import com.created.team201.presentation.common.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class CertificationCheckActivity :
+    BindingActivity<ActivityCertificationCheckBinding>(R.layout.activity_certification_check) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
