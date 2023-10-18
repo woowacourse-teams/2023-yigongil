ceff77c7bf2977752bb6bf83bd522a0b19c35f6b
Yerin <myheaven_b@naver.com>
2023-10-19 03:17:17 +0900

    feat: 시작 대기중 chip 추가 및 chip 색상 조정

--- android/app/src/main/java/com/created/team201/presentation/studyList/model/StudyListFilter.kt
+++ android/app/src/main/java/com/created/team201/presentation/studyList/model/StudyListFilter.kt
@@ -1,5 +1,5 @@
 package com.created.team201.presentation.studyList.model
 
 enum class StudyListFilter {
-    ALL, RECRUITING, PROCESSING, WAITING
+    ALL, RECRUITING, PROCESSING, WAITING, ACCEPTED
 }
