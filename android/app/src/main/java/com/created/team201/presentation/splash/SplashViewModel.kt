a9ed94d8a92ce7ccf274641b865fdea00f7bbc1d
inseonyun <yuninseon@naver.com>
2023-10-18 14:55:41 +0900

    chore: viewModel tryLogin Failed 분기 수정

--- android/app/src/main/java/com/created/team201/presentation/splash/SplashViewModel.kt
+++ android/app/src/main/java/com/created/team201/presentation/splash/SplashViewModel.kt
@@ -58,6 +58,8 @@ class SplashViewModel @Inject constructor(
                         } else {
                             _loginState.value = FAIL
                         }
+                    } else {
+                        _loginState.value = FAIL
                     }
                 }
 
