7d89264bc6521a075678fce4a555a87b91789458
Yerin <myheaven_b@naver.com>
2023-10-19 02:29:03 +0900

    fix: 멀티파트.. 어노테이션 달기

--- android/app/src/main/java/com/created/team201/data/remote/api/ImageService.kt
+++ android/app/src/main/java/com/created/team201/data/remote/api/ImageService.kt
@@ -2,11 +2,13 @@ package com.created.team201.data.remote.api
 
 import okhttp3.MultipartBody
 import retrofit2.Response
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
 interface ImageService {
 
+    @Multipart
     @POST("v1/images")
     suspend fun postImage(
         @Part imageUrl: MultipartBody.Part,
--- android/app/src/main/java/com/created/team201/presentation/certification/CertificationActivity.kt
+++ android/app/src/main/java/com/created/team201/presentation/certification/CertificationActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.viewModels
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.FileProvider
+import androidx.core.net.toUri
 import com.created.team201.R
 import com.created.team201.databinding.ActivityCertificationBinding
 import com.created.team201.presentation.certification.image.toAdjustImageFile
@@ -133,7 +134,7 @@ class CertificationActivity :
     private fun setupPostButtonListener() {
         binding.tvCertificationPostButton.setOnClickListener {
             val studyId = intent.getLongExtra(KEY_STUDY_ID, KEY_NOT_FOUND_STUDY_ID)
-            imageUri?.toAdjustImageFile(this)?.let { file ->
+            certificationViewModel.uiState.value.imageUrl.toUri().toAdjustImageFile(this)?.let { file ->
                 certificationViewModel.updateCertification(file, studyId)
             }
         }
