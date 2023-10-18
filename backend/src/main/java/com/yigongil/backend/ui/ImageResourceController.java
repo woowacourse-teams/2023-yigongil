ca41d124e79d12c718675bb342aecb0d5cee065d
bjk1649 <qhwns1649@naver.com>
2023-10-18 22:21:03 +0900

    fix: url 수정

--- backend/src/main/java/com/yigongil/backend/ui/ImageResourceController.java
+++ backend/src/main/java/com/yigongil/backend/ui/ImageResourceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @Profile(value = {"prod", "dev"})
-@RestController("/v1/images")
+@RestController("/images")
 public class ImageResourceController {
 
     private final ImageResourceService imageResourceService;
