01e740cd15103a3707b8d9fb03a7bc23e0fd9754
Bojun Kim <qhwns1649@naver.com>
2023-10-19 14:32:17 +0900

    qa 버그 수정 버전 배포 (#548)
    * fix: collection에서 원소를 삭제하지 않도록 수정 (#537)
    
    * hotfix: 이미지 컨트롤러 url 매핑 수정 (#539)
    
    * fix: collection에서 원소를 삭제하지 않도록 수정
    
    * fix: ImageResourceController url매핑 수정
    
    * fix : 홈에서 해당 스터디의 누적잔디 조회 가능 (#544)
    
    * refactor: 누적 잔디 조회 기능 수정
    
    * chore: todo 제거
    
    * fix: 클라이언트 반환 이미지 경로 수정 (#546)
    
    Co-authored-by: Bojun Kim <qhwns1649@naver.com>
    
    ---------
    
    Co-authored-by: kevstevie <109793396+kevstevie@users.noreply.github.com>
    Co-authored-by: Jae Min Yu <woawoa0513@gmail.com>

--- backend/src/main/java/com/yigongil/backend/domain/studymember/StudyMember.java
+++ backend/src/main/java/com/yigongil/backend/domain/studymember/StudyMember.java
@@ -86,12 +86,16 @@ public class StudyMember extends BaseEntity {
     }
 
     public void completeSuccessfully() {
+        int totalExperience = calculateAccumulatedExperience();
+        member.addExperience(totalExperience);
+        this.studyResult = StudyResult.SUCCESS;
+    }
+
+    public int calculateAccumulatedExperience() {
         int successfulRoundCount = study.calculateSuccessfulRoundCount(member);
         int defaultRoundExperience = EXPERIENCE_BASE_UNIT * 2;
         int additionalExperienceOfPeriodLength = EXPERIENCE_BASE_UNIT * 3 / study.getMeetingDaysCountPerWeek() + 1;
-        int totalExperience = successfulRoundCount * (defaultRoundExperience + additionalExperienceOfPeriodLength);
-        member.addExperience(totalExperience);
-        this.studyResult = StudyResult.SUCCESS;
+        return successfulRoundCount * (defaultRoundExperience + additionalExperienceOfPeriodLength);
     }
 
     public void failStudy() {
