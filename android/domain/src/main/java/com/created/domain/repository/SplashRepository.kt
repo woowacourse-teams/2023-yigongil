dd8fa9c0866c1b0b4f3588d81e92e28e96e4911b
inseonyun <yuninseon@naver.com>
2023-10-19 19:37:52 +0900

    fix: 버전 비교 model 난독화 해제

--- android/domain/src/main/java/com/created/domain/repository/SplashRepository.kt
+++ android/domain/src/main/java/com/created/domain/repository/SplashRepository.kt
@@ -3,5 +3,5 @@ package com.created.domain.repository
 import com.created.domain.model.AppUpdateInformation
 
 interface SplashRepository {
-    fun getAppUpdateInformation(versionCode: Int, onSuccess: (AppUpdateInformation) -> Unit)
+    fun getAppUpdateInformation(versionCode: Int, onAppUpdateInformationEvent: (AppUpdateInformation) -> Unit)
 }
