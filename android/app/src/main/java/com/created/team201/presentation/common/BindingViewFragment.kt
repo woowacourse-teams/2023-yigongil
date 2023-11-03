54c3a0f26e44bb60bc1d6c30f5433e700e2f4d26
s9hn <tnghqn@naver.com>
2023-11-03 15:55:43 +0900

    refactor: 새로운 ViewBinding클래스 생성

---
+++ android/app/src/main/java/com/created/team201/presentation/common/BindingViewFragment.kt
@@ -0,0 +1,30 @@
+package com.created.team201.presentation.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.viewbinding.ViewBinding
+import com.created.team201.R
+
+abstract class BindingViewFragment<VB : ViewBinding>(
+    private val bindingInflater: (LayoutInflater) -> VB
+) : Fragment() {
+    private var _binding: VB? = null
+    val binding get() = _binding ?: error(R.string.error_baseFragment)
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = bindingInflater(inflater)
+        return binding.root
+    }
+    
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
