602a632d754e17d517b0e87d160d01981c8afc39
inseonyun <yuninseon@naver.com>
2023-11-21 13:28:05 +0900

    refactor: FirstFragment DataBinding -> ViewBinding

--- android/app/src/main/java/com/created/team201/presentation/createStudy/FirstCreateStudyFragment.kt
+++ android/app/src/main/java/com/created/team201/presentation/createStudy/FirstCreateStudyFragment.kt
@@ -9,34 +9,79 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.created.team201.R
 import com.created.team201.databinding.FragmentFirstCreateStudyBinding
-import com.created.team201.presentation.common.BindingFragment
+import com.created.team201.presentation.common.BindingViewFragment
 import com.created.team201.presentation.createStudy.bottomSheet.CycleBottomSheetFragment
 import com.created.team201.presentation.createStudy.bottomSheet.PeopleCountBottomSheetFragment
 import com.created.team201.presentation.createStudy.bottomSheet.StudyDateBottomSheetFragment
+import com.created.team201.util.collectOnStarted
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class FirstCreateStudyFragment :
-    BindingFragment<FragmentFirstCreateStudyBinding>(R.layout.fragment_first_create_study) {
+    BindingViewFragment<FragmentFirstCreateStudyBinding>(FragmentFirstCreateStudyBinding::inflate) {
     private val createStudyViewModel: CreateStudyViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initBinding()
+        setupPeopleCount()
+        setupStudyDate()
+        setupStudyCycle()
+        setupNextClick()
         setupCollectEnableNext()
     }
 
-    private fun initBinding() {
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.viewModel = createStudyViewModel
-        binding.onInputClickListener = ::setOnInputClick
+    private fun setupPeopleCount() {
+        createStudyViewModel.peopleCount.collectOnStarted(viewLifecycleOwner) { peopleCount ->
+            if (peopleCount == -1) return@collectOnStarted
+
+            binding.tvFirstCreateStudyPeopleCount.text =
+                getString(
+                    R.string.first_create_study_formatter_information_people_count,
+                    peopleCount
+                )
+        }
+
+        binding.tvFirstCreateStudyPeopleCount.setOnClickListener(
+            setOnInputClick(TAG_CREATE_STUDY_PEOPLE_COUNT)
+        )
     }
 
+    private fun setupStudyDate() {
+        createStudyViewModel.studyDate.collectOnStarted(viewLifecycleOwner) { studyDate ->
+            if (studyDate == -1) return@collectOnStarted
 
-    private fun setOnInputClick(tag: String) {
+            binding.tvFirstCreateStudyStudyDate.text =
+                getString(R.string.first_create_study_formatter_information_study_date, studyDate)
+        }
+
+        binding.tvFirstCreateStudyStudyDate.setOnClickListener(
+            setOnInputClick(TAG_CREATE_STUDY_STUDY_DATE)
+        )
+    }
+
+    private fun setupStudyCycle() {
+        createStudyViewModel.cycle.collectOnStarted(viewLifecycleOwner) { cycle ->
+            if (cycle == -1) return@collectOnStarted
+
+            binding.tvFirstCreateStudyCycle.text =
+                getString(R.string.first_create_study_formatter_information_cycle, cycle)
+        }
+
+        binding.tvFirstCreateStudyCycle.setOnClickListener(
+            setOnInputClick(TAG_CREATE_STUDY_CYCLE)
+        )
+    }
+
+    private fun setupNextClick() {
+        binding.tvFirstCreateStudyBtnNext.setOnClickListener {
+            createStudyViewModel.navigateToNext()
+        }
+    }
+
+    private fun setOnInputClick(tag: String): View.OnClickListener = View.OnClickListener {
         removeAllFragment()
         createBottomSheetFragment(tag)?.show(childFragmentManager, tag)
     }
@@ -51,21 +96,10 @@ class FirstCreateStudyFragment :
 
     private fun createBottomSheetFragment(tag: String): BottomSheetDialogFragment? {
         return when (tag) {
-            getString(R.string.create_study_tag_people_count) -> {
-                PeopleCountBottomSheetFragment()
-            }
-
-            getString(R.string.create_study_tag_study_date) -> {
-                StudyDateBottomSheetFragment()
-            }
-
-            getString(R.string.create_study_tag_cycle) -> {
-                CycleBottomSheetFragment()
-            }
-
-            else -> {
-                null
-            }
+            TAG_CREATE_STUDY_PEOPLE_COUNT -> PeopleCountBottomSheetFragment()
+            TAG_CREATE_STUDY_STUDY_DATE -> StudyDateBottomSheetFragment()
+            TAG_CREATE_STUDY_CYCLE -> CycleBottomSheetFragment()
+            else -> null
         }
     }
 
@@ -77,5 +111,11 @@ class FirstCreateStudyFragment :
                 }
             }
         }
+    }
+
+    companion object {
+        private const val TAG_CREATE_STUDY_PEOPLE_COUNT = "TAG_CREATE_STUDY_PEOPLE_COUNT"
+        private const val TAG_CREATE_STUDY_STUDY_DATE = "TAG_CREATE_STUDY_STUDY_DATE"
+        private const val TAG_CREATE_STUDY_CYCLE = "TAG_CREATE_STUDY_CYCLE"
     }
 }
