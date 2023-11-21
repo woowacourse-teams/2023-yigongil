e9b0b526a99194d43afc38295fe11bb541b63ed8
inseonyun <yuninseon@naver.com>
2023-11-21 14:00:26 +0900

    refactor: StudyDateBottomSheetFragment DataBinding -> ViewBinding

--- android/app/src/main/java/com/created/team201/presentation/createStudy/bottomSheet/StudyDateBottomSheetFragment.kt
+++ android/app/src/main/java/com/created/team201/presentation/createStudy/bottomSheet/StudyDateBottomSheetFragment.kt
@@ -1,34 +1,58 @@
 package com.created.team201.presentation.createStudy.bottomSheet
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import com.created.team201.R
 import com.created.team201.databinding.FragmentStudyDateBottomSheetBinding
-import com.created.team201.presentation.common.BindingBottomSheetFragment
+import com.created.team201.presentation.common.BindingViewBottomSheetFragment
 import com.created.team201.presentation.createStudy.CreateStudyViewModel
+import com.created.team201.util.collectOnStarted
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class StudyDateBottomSheetFragment :
-    BindingBottomSheetFragment<FragmentStudyDateBottomSheetBinding>(
-        R.layout.fragment_study_date_bottom_sheet,
+    BindingViewBottomSheetFragment<FragmentStudyDateBottomSheetBinding>(
+        FragmentStudyDateBottomSheetBinding::inflate,
     ) {
     private val createStudyViewModel: CreateStudyViewModel by activityViewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        dialog?.let {
+            val behavior = (it as BottomSheetDialog).behavior
+            behavior.state = BottomSheetBehavior.STATE_EXPANDED
+            it.setCanceledOnTouchOutside(false)
+        }
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initBinding()
+        setupSaveButtonClick()
+        setupCancelButtonClick()
+        collectPeopleCount()
     }
 
-    private fun initBinding() {
-        binding.viewModel = createStudyViewModel
-        binding.onCancelClickListener = { dismiss() }
-        binding.onSaveClickListener = ::onSaveButtonClick
-        binding.lifecycleOwner = viewLifecycleOwner
+    private fun setupSaveButtonClick() {
+        binding.tvStudyDateBottomSheetBtnSave.setOnClickListener {
+            createStudyViewModel.setStudyDate(binding.spStudyDate.value)
+            dismiss()
+        }
     }
 
-    private fun onSaveButtonClick() {
-        createStudyViewModel.setStudyDate(binding.spStudyDate.value)
-        dismiss()
+    private fun setupCancelButtonClick() {
+        binding.tvStudyDateBottomSheetBtnCancel.setOnClickListener { dismiss() }
+    }
+
+    private fun collectPeopleCount() {
+        createStudyViewModel.studyDate.collectOnStarted(viewLifecycleOwner) { studyDate ->
+            binding.spStudyDate.setValue(studyDate)
+        }
     }
 }
